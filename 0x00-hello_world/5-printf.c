a3f912d07a7d3198a464f2e5e9b7dd8a06e104d7
Ukwuezepascaline <ezikepascaline@gmail.com>
2022-09-08 09:16:02 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
 /**
- * main - entry point
- * Return: 0
+ * main - Entry point
+ *
+ * Return: always 0
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
+}
