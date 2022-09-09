3618fbd1ab3740b0401e72dd22c14389b2b3aef6
Ukwuezepascaline <ezikepascaline@gmail.com>
2022-09-09 11:07:12 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: always 0
- */
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
