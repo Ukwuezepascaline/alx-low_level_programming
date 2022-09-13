a70637e6365943ef956413de40cb91937c628740
Ukwuezepascaline <ezikepascaline@gmail.com>
2022-09-13 23:52:28 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * main -  prints _putchar, followed by a new line
- * Return: Always 0 (Success)
- */
+* main - Print the word "_putchar"
+*
+* Return: Always 0
+*/
+
 int main(void)
 {
-	putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(p[i]);
+	}
 	_putchar('\n');
 
 	return (0);
