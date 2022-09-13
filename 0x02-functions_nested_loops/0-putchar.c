b0a0d40a8f4b6b330658b62bbdec8ed7e25f3ab6
Ukwuezepascaline <ezikepascaline@gmail.com>
2022-09-13 15:40:46 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - print putchar
- * Return: always 0
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	putchar('');
@@ -18,5 +18,4 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
-
 }
