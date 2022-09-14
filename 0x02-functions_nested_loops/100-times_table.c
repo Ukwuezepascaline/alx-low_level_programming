349f71dc8832aded70487393c4b1e973ea40d632
Ukwuezepascaline <ezikepascaline@gmail.com>
2022-09-13 18:34:09 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -37,4 +37,11 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 1…
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
