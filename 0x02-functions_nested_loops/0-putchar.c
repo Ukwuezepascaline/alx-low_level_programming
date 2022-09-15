6aaaf3ba1c707b613a28897b15fa40dacbf8ac40
Ukwuezepascaline <ezikepascaline@gmail.com>
2022-09-15 02:53:59 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,24 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - print putchar
+ * main - entry point
+ *
+ * Description: Prints Holberton with _putchar
+ *
  * Return: always 0
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	int i = 0;
+	char c;
+	char s[] = "Holberton\n";
 
+	while (i <= 9)
+	{
+		c = s[i];
+		_putchar(c);
+		i++;
+	}
 	return (0);
-
 }
