49fc299478da56f7348b3f1e5af15006cfd67c69
Ukwuezepascaline <ezikepascaline@gmail.com>
2022-09-29 23:06:11 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,47 +1,43 @@
 #include "main.h"
 
-int check_pal(char *s, int i, int len);
-int _strlen_recursion(char *s);
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n);
 
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to reverse
+ * find_sqrt - Finds the natural square root of an inputted number.
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
  *
- * Return: 1 if it is, 0 it's not
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
  */
-int is_palindrome(char *s)
+int find_sqrt(int num, int root)
 {
-	if (*s == 0)
-		return (1);
-	return (check_pal(s, 0, _strlen_recursion(s)));
+	if ((root * root) == num)
+		return (root);
+
+	if (root == num / 2)
+		return (-1);
+
+	return (find_sqrt(num, root + 1));
 }
 
 /**
- * _strlen_recursion - returns the length of a string
- * @s: string to calculate the length of
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to return the square root of.
  *
- * Return: length of the string
+ * Return: If n has a natural square root - the natural square root of n.
+ *         If n does not have a natural square root - -1.
  */
-int _strlen_recursion(char *s)
+int _sqrt_recursion(int n)
 {
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
-}
+	int root = 0;
 
-/**
- * check_pal - checks the characters recursively for palindrome
- * @s: string to check
- * @i: iterator
- * @len: length of the string
- *
- * Return: 1 if palindrome, 0 if not
- */
-int check_pal(char *s, int i, int len)
-{
-	if (*(s + i) != *(s + len - 1))
-		return (0);
-	if (i >= len)
+	if (n < 0)
+		return (-1);
+
+	if (n == 1)
 		return (1);
-	return (check_pal(s, i + 1, len - 1));
+
+	return (find_sqrt(n, root));
 }
