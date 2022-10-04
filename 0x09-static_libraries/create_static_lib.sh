7de01b77cb217c84d58684cbe4044a6dbb227bf6
Ukwuezepascaline <ezikepascaline@gmail.com>
2022-10-04 13:38:39 -0700

    task 2

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,19 @@
-#!/bin/bash
+#include "main.h"
+#include <stdlib.h>
 
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-
-ar rc liball.a *.o
-
-ranlib liball.a
+/**
+ * free_grid - frees a 2 dimensional grid.
+ * @grid: multidimensional array of integers.
+ * @height: height of the grid.
+ *
+ * Return: no return
+ */
+void free_grid(int **grid, int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
+}
