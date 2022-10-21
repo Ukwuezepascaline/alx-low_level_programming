c16d81d663e52a4eb6e1337a9e1e4174dd0ca850
Ukwuezepascaline <ezikepascaline@gmail.com>
2022-10-21 02:37:35 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,25 +1,13 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#include "lists.h"
+
+void __attribute__((constructor)) hare(void);
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * hare - Prints a string before the
+ *   main function is executed.
  */
-int main(void)
+void hare(void)
 {
-    list_t *head;
-
-    head = NULL;
-    add_node_end(&head, "Bob");
-    add_node_end(&head, "&");
-    add_node_end(&head, "Kris");
-    add_node_end(&head, "love");
-    add_node_end(&head, "asm");
-    print_list(head);
-    free_list(head);
-    head = NULL;
-    return (0);
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
 }
